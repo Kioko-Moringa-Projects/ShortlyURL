c69fb6b2cb3193893174051599fdf676259b656b
Calvin <calvinkioko91@gmail.com>
2022-10-11 20:41:53 +0300

    Milestone: Modified users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,20 @@
 class UsersController < ApplicationController
+    skip_before_action :authenticate, only: [:create]
+
+    def create
+        user = User.create!(user_params)
+        session[:user_id] = user.id
+        render json: user, status: :created
+    end
+
+    def show
+        user = User.find(session[:user_id])
+        render json: user, status: :created
+    end
+
+    private
+
+    def user_params
+        params.permit(:first_name, :last_name, :email, :username, :password, :password_confirmation)
+    end
 end
