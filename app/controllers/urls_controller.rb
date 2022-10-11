66e23dbf07957cbee87daed9243d6d877d8bdb1d
Calvin <calvinkioko91@gmail.com>
2022-10-11 20:40:42 +0300

    Milestone: Modified Urls controller

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -1,2 +1,19 @@
 class UrlsController < ApplicationController
+    
+    def index
+        urls = Url.all
+        render json: urls, status: :created
+    end
+
+    def create
+        user = Url.find(session[:user_id])
+        urls = user.urls.create!(urls_params)
+        render json: urls, status: :created
+    end
+
+    private
+
+    def urls_params
+        params.permit(:original_url, :shortly_url)
+    end
 end
