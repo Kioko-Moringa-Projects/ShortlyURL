b96b9cec9a2af96c1222a011ddba6b30471722d7
Calvin <calvinkioko91@gmail.com>
2022-10-11 15:10:10 +0300

    Milestone: Added columns for joint table

--- db/migrate/20221011101213_create_user_urls.rb
+++ db/migrate/20221011101213_create_user_urls.rb
@@ -1,6 +1,8 @@
 class CreateUserUrls < ActiveRecord::Migration[6.1]
   def change
     create_table :user_urls do |t|
+      t.integer :user_id
+      t.integer :url_id
 
       t.timestamps
     end
