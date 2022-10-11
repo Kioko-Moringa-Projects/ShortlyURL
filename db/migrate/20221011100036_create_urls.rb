737a1f3c8b62fb76b7a0f3dd6f6949cda0ba5ad2
Calvin <calvinkioko91@gmail.com>
2022-10-11 15:09:47 +0300

    Milestone: Added columns for URLs

--- db/migrate/20221011100036_create_urls.rb
+++ db/migrate/20221011100036_create_urls.rb
@@ -1,6 +1,8 @@
 class CreateUrls < ActiveRecord::Migration[6.1]
   def change
     create_table :urls do |t|
+      t.string :original_url
+      t.string :shortly_url
 
       t.timestamps
     end
