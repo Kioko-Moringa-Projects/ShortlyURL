340cc01879a26f9d5736a8d3d6fcb7d85e413614
Calvin <calvinkioko91@gmail.com>
2022-10-14 06:48:31 +0300

    Edited routes, schema and seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_11_100036) do
+ActiveRecord::Schema.define(version: 2022_10_14_000307) do
+
+  create_table "customers", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "username"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "urls", force: :cascade do |t|
+    t.integer "user_id"
     t.string "original_url"
     t.string "shortly_url"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "user_urls", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "🌱 Seeding data..."
+#Randomizing array examples: ['Area of Dmg', 'Single target Dmg', 'Speed training', 'Burst Dmg', 'Core Attr training'].sample
+100.times do
+    seeded_customers = Customer.create(
+        first_name: Faker::Name.first_name,
+        last_name: Faker::Name.last_name,
+        username: Faker::Name.last_name,
+        email: Faker::Internet.email,
+        password_digest: "calvin",
+        
+    )
+end
